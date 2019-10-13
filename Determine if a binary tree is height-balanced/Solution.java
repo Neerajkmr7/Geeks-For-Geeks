6f6c7da4191b5317d0a3e956f3e37583a27dda72
Neeraj Kumar <neerajkmr7@gmail.com>
2019-10-13 11:24:09 -0700

    Update Solution.java

--- Determine if a binary tree is height-balanced/Solution.java
+++ Determine if a binary tree is height-balanced/Solution.java
@@ -17,3 +17,26 @@ public class Solution {
         return 1+Math.min(minDepth(root.left),minDepth(root.right));
     }
 }
+
+/*
+class Solution {
+    private boolean check = true;
+    public boolean isBalanced(TreeNode root) {
+        helper(root);
+        return check;
+    }
+    
+    private int helper(TreeNode root){
+        if(root == null) return 0;
+        int left = helper(root.left);
+        int right = helper(root.right);
+        int diff = Math.abs(left - right);
+        
+        if(diff > 1) {
+            check = false;
+        }
+        
+        return 1 + Math.max(left, right);
+    }
+}
+*/
